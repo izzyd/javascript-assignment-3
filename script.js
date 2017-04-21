da20986f845cc4b1cd66a5004d2da6174dd18acc
Isabel Dec <idec@id.iit.edu>
2017-04-21 10:35:29 -0500

    assignment done.

--- script.js
+++ script.js
@@ -22,11 +22,11 @@
 // Set up canvas v2!? 
 
 
-document.body.onload = addElement;
+// document.body.onload = addElement;
 
-function addElement() {
-    // create a new div element 
-    // and give it some content 
+// function addElement() {
+//     // create a new div element 
+//     // and give it some content 
     var view = document.createElement("div");
     view.className = "view";
     document.body.appendChild(view);
@@ -39,50 +39,66 @@ function addElement() {
     sun.className = "sun";
     view.appendChild(sun);
 
-    //   // add the newly created element and its content into the DOM ?
-    //   var currentDiv = document.getElementById("grass"); 
-    //   document.body.insertBefore(newDiv, currentDiv); 
-
-    //     var currentDiv = document.getElementById("sun"); 
-    //   document.body.insertBefore(newDiv, currentDiv); 
-}
-
-// while loop 
-var ray1 = document.createElement("div");
-ray1.className = "ray1";
-sun.appendChild(ray1);
-//view.appendChild(ray1);
-// view.appendChild(sun); 
-
-var ray2 = document.createElement("div");
-ray2.className = "ray2";
-sun.appendChild(ray2);
-
-var ray3 = document.createElement("div");
-ray2.className = "ray3";
-sun.appendChild(ray3);
-
-var ray4 = document.createElement("div");
-ray2.className = "ray4";
-sun.appendChild(ray4);
-
-var ray5 = document.createElement("div");
-ray2.className = "ray5";
-sun.appendChild(ray5);
-
+// // while loop 
 var i = 0;
-while (i < 1) {
-    sun.appendChild(ray1);
-    sun.appendChild(ray2);
-    sun.appendChild(ray3);
-    sun.appendChild(ray4);
-    sun.appendChild(ray5);
+while (i < 5) {
+    var ray = document.createElement("div");
+    ray.className = "ray-" + (i + 1);
+    sun.appendChild(ray);
     i++;
 }
-//hmm...rays not working :/ 
-
 
 var mountain = document.createElement("div");
 mountain.className = "mountain";
 view.appendChild(mountain); 
+
+var mountainTop = document.createElement("div");
+mountainTop.className = "mountain-top";
+mountain.appendChild(mountainTop);
+
+var i = 0;
+do {
+    var mountainCap = document.createElement("div");
+    mountainCap.className = "mountain-cap-" + (i + 1);
+    mountainTop.appendChild(mountainCap);
+    i++
+} while (i < 3);
+
+
+
+// // while loop -- troubleshoot
+// var ray1 = document.createElement("div");
+// ray1.className = "ray1";
+// sun.appendChild(ray1);
+// //view.appendChild(ray1);
+// // view.appendChild(sun); 
+
+// var ray2 = document.createElement("div");
+// ray2.className = "ray2";
+// sun.appendChild(ray2);
+
+// var ray3 = document.createElement("div");
+// ray2.className = "ray3";
+// sun.appendChild(ray3);
+
+// var ray4 = document.createElement("div");
+// ray2.className = "ray4";
+// sun.appendChild(ray4);
+
+// var ray5 = document.createElement("div");
+// ray2.className = "ray5";
+// sun.appendChild(ray5);
+
+// var i = 0;
+// while (i < 1) {
+//     sun.appendChild(ray1);
+//     sun.appendChild(ray2);
+//     sun.appendChild(ray3);
+//     sun.appendChild(ray4);
+//     sun.appendChild(ray5);
+//     i++;
+// }
+//hmm...rays not working :/ 
+
+
 

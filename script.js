70065032076b3640dae02370cf061ba755f83db2
Isabel Dec <idec@id.iit.edu>
2017-04-20 22:56:20 -0500

    view, grass, and sun elements on the page!

--- script.js
+++ script.js
@@ -15,9 +15,11 @@
 //   document.body.insertBefore(newDiv, currentDiv); 
 // }
 
-// Set up!?
-var a_canvas = document.getElementById("a");
-var context = a_canvas.getContext("2d");
+// // Set up canvas!?
+// var a_canvas = document.getElementById("a");
+// var context = a_canvas.getContext("2d");
+
+// Set up canvas v2!? 
 
 
 document.body.onload = addElement;
@@ -26,18 +28,23 @@ function addElement () {
   // create a new div element 
   // and give it some content 
   var view = document.createElement("div"); 
+  view.className = "view";
   document.body.appendChild(view); 
-  var grass = document.createElementNS("grass"); 
+
+  var grass = document.createElement("div"); 
+  grass.className = "grass";
   view.appendChild(grass); //add the...to the newly created div. 
-  var sun = document.createElementNS("sun");
+
+  var sun = document.createElement("div");
+  sun.className = "sun";
   view.appendChild(sun); 
 
-  // add the newly created element and its content into the DOM 
-  var currentDiv = document.getElementById("grass"); 
-  document.body.insertBefore(newDiv, currentDiv); 
+//   // add the newly created element and its content into the DOM ?
+//   var currentDiv = document.getElementById("grass"); 
+//   document.body.insertBefore(newDiv, currentDiv); 
 
-    var currentDiv = document.getElementById("sun"); 
-  document.body.insertBefore(newDiv, currentDiv); 
+//     var currentDiv = document.getElementById("sun"); 
+//   document.body.insertBefore(newDiv, currentDiv); 
 }
 
 

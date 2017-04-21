2313e263959f7567ff846300711bd0c514c681b1
Isabel Dec <idec@id.iit.edu>
2017-04-20 20:43:51 -0500

    Setting up variables.

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
 // Do your work in this file.
 // ok. 
 
+// document.body.onload = addElement;
+
+// function addElement () { 
+//   // create a new div element 
+//   // and give it some content 
+//   var newDiv = document.createElement("div"); 
+//   var newContent = document.createTextNode("Hi there and greetings!"); 
+//   newDiv.appendChild(newContent); //add the text node to the newly created div. 
+
+//   // add the newly created element and its content into the DOM 
+//   var currentDiv = document.getElementById("div1"); 
+//   document.body.insertBefore(newDiv, currentDiv); 
+// }
+
+document.body.onload = addElement;
+
+function addElement () { 
+  // create a new div element 
+  // and give it some content 
+  var view = document.createElement("div"); 
+  document.body.appendChild(view); 
+  var grass = document.createElement("grass"); 
+  view.appendChild(grass); //add the...to the newly created div. 
+  var sun = document.createElement("sun");
+  view.appendChild(sun); 
+
+//   // add the newly created element and its content into the DOM 
+//   var currentDiv = document.getElementById("div1"); 
+//   document.body.insertBefore(newDiv, currentDiv); 
+}
+
 
 
